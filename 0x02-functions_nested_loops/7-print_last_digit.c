03154f21ad568fdca35925212910045a1e9b5ef6
ezophebe <ezophebe91@gmail.com>
2023-09-13 17:10:44 -0700

    eighth commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_last_digit - it prints the lst digit
+*@i: declared parameter
+*Return: j
+*/
+int print_last_digit(int i)
+{
+	int j;
+
+	j = i % 10;
+
+	if (i < 0)
+	{
+		j = -j;
+		_putchar(j + '0');
+	}
+
+	return (j);
+}
