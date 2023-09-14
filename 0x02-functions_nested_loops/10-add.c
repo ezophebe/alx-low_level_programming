b33f56f2f28ee3a9515c5d5c857033cab08ec842
ezophebe <ezophebe91@gmail.com>
2023-09-13 19:42:32 -0700

    eleventh commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add - This function adds two integer nd return the value
+*@a: declared parameter one
+*@b: declared parameter two
+*Return: a+b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
