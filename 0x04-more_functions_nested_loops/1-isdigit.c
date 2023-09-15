e5f0e67d8cb55326f431f53c01468663900e0745
ezophebe <ezophebe91@gmail.com>
2023-09-15 15:29:41 +0000

    third commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isdigit - checks for a digit (o - 9)
+*@c: declared parameter
+*Return: always 1 on true, 0 on false
+*/
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul - a function that multiplies two integers
+*@a: declared parameter
+*@b: declared parameter
+*Return: a * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
