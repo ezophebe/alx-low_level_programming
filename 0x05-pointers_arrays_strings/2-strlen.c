788ba651086829ebce25d65db7d4a027f1c60fda
ezophebe <ezophebe91@gmail.com>
2023-09-19 00:54:17 +0000

    third commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - A function that returns the lenght of a string
+ * @s: declared char
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; *s++;)
+		i++;
+	return (i);
+}
