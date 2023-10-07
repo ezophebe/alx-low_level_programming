d729b1a5a7b2d2ec21590d693e8b7ae0d8d49d8b
ezophebe <ezophebe91@gmail.com>
2023-10-07 23:08:44 +0000

    static lib

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - This funtion concatenates two strings
+ * @dest: declared parameter 1
+ * @src: declared parameter 2
+ * @n: declared parameter 3
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l = strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n && *src != '\0' ; i++)
+	{
+		dest[l + i] = *src;
+		src++;
+	}
+		dest[l + i] = '\0';
+	return (dest);
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_isalpha - This function checks for alphabetic character.
+ *@c: parameter declared
+ *Return: 1 and 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+}
