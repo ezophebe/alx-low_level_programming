e9e506a8da89ad5a19427fc1b4039a7d105815fa
ezophebe <ezophebe91@gmail.com>
2023-10-14 01:37:19 +0000

    2nd

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - print name using pointer to function
- * @name: string to add
+ * print_name - function that prints a name using a pointer to function
+ * @name: string to add together
  * @f: pointer to function
- * Return: nothing
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
