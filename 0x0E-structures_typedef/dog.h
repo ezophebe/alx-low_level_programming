2a79ec2c2432397b0d3218ca4d2c611849781f6d
ezophebe <ezophebe91@gmail.com>
2023-10-16 09:43:39 +0000

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - type declared
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: name of the owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
