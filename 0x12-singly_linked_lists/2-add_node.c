a05b3ac0e91f0db3f0028b9b53778e9a1c1589bb
ezophebe <ezophebe91@gmail.com>
2023-10-20 02:33:53 +0000

    func adds a new node at the begining of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node- functn adds a new node at the beginning of list
+ *@head: pointer to head of node
+ *@str: string to br printd
+ *Return: new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newelem;
+	unsigned int i, count;
+
+	count = 0;
+
+	newelem = malloc(sizeof(list_t));
+	if (newelem == NULL)
+		return (NULL);
+	newelem->str = strdup(str);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	newelem->len = count;
+	newelem->next = *head;
+	*head = newelem;
+	return (*head);
+}
