7a130c83ea3e96173b3754e40547d6313cb7fda4
ezophebe <ezophebe91@gmail.com>
2023-10-24 13:56:02 +0000

    fuction prints a linked lists

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,65 @@
+#include "lists.h"
+
+/**
+ *free_listp - free a linked list
+ *@head: pointer to pointer of the head
+ *Return: void
+ */
+
+void free_listp(listp_t **head)
+{
+	listp_t *tempo;
+	listp_t *c;   /**current*/
+
+	if (head != NULL)
+	{
+		c = *head;
+	while ((tempo = c) != NULL)
+	{
+		c = c->next;
+		free(tempo);
+	}
+	*head = NULL;
+	}
+}
+
+/**
+ *print_listint_safe - function prints a list
+ *@head: pointer to head
+ *Return: num of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = 0;
+	listp_t *p_head, *add, *neww;
+
+	p_head = NULL;
+	while (head != NULL)
+	{
+		neww = malloc(sizeof(listp_t));
+
+		if (neww == NULL)
+			exit(98);
+
+		neww->p = (void *) head;
+		neww->next = p_head;
+		p_head = neww;
+		add = p_head;
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *) head, head->n);
+				free_listp(&p_head);
+				return (nodes);
+			}
+		}
+		printf("-> [%p] %d\n", (void *) head, head->n);
+		head = head->next;
+		nodes++;
+	}
+	free_listp(&p_head);
+	return (nodes);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,18 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ *struct listp_s - dingly linked list
+ *@p: pointer to p
+ *@next: points to next node
+ *Description: singly list
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
