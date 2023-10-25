008f3244c90c62495f01cb8407ad3c8ae4641ecb
Ezo Phebe <135470134+ezophebe@users.noreply.github.com>
2023-10-25 08:19:34 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,34 +1,37 @@
 #include "lists.h"
 /**
- *add_node_end - funtion adds a new node at the end of a list
- *@head: pointer to head of node
- *@str: string to the printed
- *Return: pointer to the new node
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: A pointer to the head of the list.
+ * @str: The string to be duplicated and added to the new node.
+ * Return: The address of the new element, or NULL if it fails.
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newelem, *tempry;
-	unsigned int i, count;
+    list_t *new_elem, *temp;
+    
+    new_elem = malloc(sizeof(list_t));
+    if (new_elem == NULL)
+        return (NULL);
 
-	count = 0;
+    new_elem->str = strdup(str);
+    if (new_elem->str == NULL) {
+        free(new_elem);
+        return (NULL);
+    }
 
-	newelem = malloc(sizeof(list_t));
-	if (newelem == NULL)
-		return (NULL);
-	newelem->str = strdup(str);
-	for (i = 0; str[i] != '\0'; i++)
-		count++;
-	newelem->len = count;
-	newelem->next = NULL;
-	tempry = *head;
+    new_elem->len = strlen(str);
+    new_elem->next = NULL;
 
-	if (tempry == NULL)
-		*head = newelem;
-	else {
-		        while (tempry->next != NULL)
-				            tempry = tempry->next;
-			        tempry->next = newelem;
-				    }
-	    return (*head);
+    if (*head == NULL) {
+        *head = new_elem;
+    } else {
+        temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_elem;
+    }
+
+    return (new_elem);
 }
+
