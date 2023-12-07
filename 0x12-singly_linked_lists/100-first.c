7803b8b2e847967ab13b9288d8f3694c6f86b157
ezophebe <ezophebe91@gmail.com>
2023-12-07 23:04:14 +0000

    tasks 3

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * first - function prints a sentence before the main
+ * Return: none
+ */
+
+void first(void)
+{
+	 printf("You're beat! and yet, you must allow,\n");
+	  printf("I bore my house upon my back!\n");
+}
