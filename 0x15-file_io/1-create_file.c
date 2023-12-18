a8bddef0077dc4fc5de88ee3f95c5e5ae44a6c53
ezophebe <ezophebe91@gmail.com>
2023-12-18 22:01:14 +0000

    function creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,9 @@ int create_file(const char *filename, char *text_content)
 	if (file_desc == -1)
 		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 	for (num_letters = 0; text_content[num_letters]; num_letters++)
+		;
 	r_write = write(file_desc, text_content, num_letters);
 	if (r_write == -1)
 		return (-1);
